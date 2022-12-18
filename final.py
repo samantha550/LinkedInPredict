1a0b79ac3b8d1f26a9b7f513e37cb2c98568d5b7
samantha550 <120540922+samantha550@users.noreply.github.com>
2022-12-18 13:34:46 -0800

    Update final.py

--- final.py
+++ final.py
@@ -51,31 +51,22 @@ st.markdown(f"### The purpose of this app is to predict the chances of you being
 st.markdown(f"#### To get started please enter your information below:")
 age = st.slider(label="Enter your age",  min_value=1, max_value=100,value=50)
 inc = st.selectbox(f"Income Range - see below for description of options", options = ["Less than $10,000", "10 to under $20,000","20 to under $30,000","30 to under $40,000","40 to under $50,000","50 to under $75,000"," 75 to under $100,000","100 to under $150,000","$150,000 or more"] )
-#st.text(f"(1 = Less than $10,000 | "
-#" 2 = 10 to under $20,000 | "
-#" 3 = 20 to under $30,000 |"
-#"4 = 30 to under $40,000 | "
-#" 5 = 40 to under $50,000 | "
-#" 6 = 50 to under $75,000 |"
-#"7 = 75 to under $100,000 | "
-#" 8 = 100 to under $150,000 | "
-#" 9 = $150,000 or more)")
 #st.number_input("Income (low=1 to high=9)", 1, 9)
-deg= st.selectbox(f"Degree Level - see below for description of options", options = [1,2,3,4,5,6,7,8] )
-st.text(f"(1 = Did not go to High School |"
-"2 = High school incomplete | "
-"3 = High school graduate or GED |"
-"4 = Some college, no degree | "
-"5 = Two-year degree - Associates |"
-"6 = Four-year degree - Bachelor |"
-"7 = Some postgraduate or professional schooling |"
-"8 = Postgraduate or professional degree (e.g., MA, MS, PhD, MD, JD))")
+deg= st.selectbox(f"Degree Level - see below for description of options", options = ["Did not go to High School","High school incomplete","High school graduate or GED","Some college, no degree","Two-year degree - Associates","Four-year degree - Bachelor","Some postgraduate or professional schooling","Postgraduate or professional degree (e.g., MA, MS, PhD, MD, JD))"] )
+#st.text(f"(1 = Did not go to High School |"
+#"2 = High school incomplete | "
+#"3 = High school graduate or GED |"
+#"4 = Some college, no degree | "
+#"5 = Two-year degree - Associates |"
+#"6 = Four-year degree - Bachelor |"
+#"7 = Some postgraduate or professional schooling |"
+#"8 = Postgraduate or professional degree (e.g., MA, MS, PhD, MD, JD))")
 #deg = st.number_input("College degree (no=0 to yes=1)", 1, 9)
 mar = st.number_input("Married (0=no, 1=yes)", 0, 1)
 par = st.number_input("Parent (0=no, 1=yes)", 0, 1)
 gen = st.number_input("Gender (0=Male, 1=Female)", 0, 1)
 
-#st.write(f"#### Based on your responses as a {age} year old {gen_label}; who is {mar_label},{par_label}, a {deg_label}, and in a {inc_label} bracket: ")
+
 
 if inc == "Less than $10,000":
      inc = 1
@@ -96,6 +87,26 @@ elif inc == "100 to under $150,000":
 else:
     inc = 9
 
+    
+if deg == "Did not go to High School":
+     deg = 1
+elif deg == "High school incomplete":
+     deg = 2
+elif deg =="High school graduate or GED":
+    deg = 3
+elif deg == "Some college, no degree":
+    deg = 4
+elif deg == "Two-year degree - Associates":
+    deg = 5
+elif deg == "Four-year degree - Bachelor":
+    deg = 6
+elif deg == "Some postgraduate or professional schooling":
+    deg = 7
+else:
+    deg = 8
+
+    
+ 
 new_guess = pd.DataFrame({
     "age": age,
     "education": [deg],
